eb462357ac3e8c094674f7a557be96ebfa4f66f0
JGunnFo <jeffgunn814@gmail.com>
2020-09-03 15:10:54 -0700

    More data added.'

--- src/premade data/articles.js
+++ src/premade data/articles.js
@@ -43,6 +43,7 @@ different upcoming movies! </div>
   Fictional Movie Business account, your favorite movie genre, and your favorite movie snack, 
   and we'll get back to you soon!</div></div>;
 
+
 /* be aware that this is a function, so theres a difference between Form1 and Form1(), reference vs call*/
 function Form1(){
     return(
@@ -113,6 +114,79 @@ function Form1(){
   }
   
 
+
+
+  
+const article3={
+  TitleText: "30% off Tickets on September 5th!",
+  PreviewText: "What could be better than cheap movies on the weekend?",
+  Date: new Date(2020, 8, 31),
+  Sections: ["HTML4"]
+  };
+
+  
+    
+const HTML4 = <div><div className="Temporary-Test">On Saturday, September 5th, all tickets will be 
+30% off! That applies to both booking in advance, day-of purchases, and digital screenings! 
+The only thing you have to do is show up and buy at least 2 buckets of popcorn.</div></div>;
+
+
+
+  
+const article4={
+  TitleText: "Updated Rules on Bringing Pets",
+  PreviewText: "If you want to bring your alpaca along, be sure to read these rules to save your movie night!",
+  Date: new Date(2020, 9, 5),
+  Sections: ["HTML5"]
+  };
+
+  
+    
+const HTML5 = <div><div className="Temporary-Test">Here at Fictional Movie Business, we've gone to great extents to allow people to bring all of their pets to our theaters, including immense effort to install animal-only areas and to provide trained staff to make sure nothing goes wrong. We love to see all your dogs, cats, lizards, alpacas, ferrets, and everything else!</div>
+<div>However, for you to have a safe and pleasant moviegoing experience, we have added 3 new rules:</div>
+<div>-All animals must be at least a year old, so that they have any hope of obeying rules.</div>
+<div>-If the movie is 3 hours or longer, you must bring snacks for our staff to feed to your pet.</div>
+<div>-No leash, no service.</div></div>;
+
+
+
+  
+const article5={
+  TitleText: "Several 1970s movies available!",
+  PreviewText: "We've added a lot of 1970s classics to our digital library!",
+  Date: new Date(2020, 8, 25),
+  Sections: ["HTML6"]
+  };
+
+  const HTML6 = <div><div className="Temporary-Test">It's a blast from the past! 
+  We've added FORTY movies from the 1970s to our digital store!</div>
+  <div>Many of them you've surely heard of- 
+  but we've also included a few obscure movies that we got the rights to for practically nothing. 
+  Enjoy!</div></div>;
+  
+  
+  
+
+  const article6={
+    TitleText: "Horror Week Sale!",
+    PreviewText: "For horror week, you can save big! Click here to find out more!",
+    Date: new Date(2020, 8, 15),
+    Sections: ["HTML7"]
+    };
+
+
+  
+    const HTML7 = <div><div className="Temporary-Test">From August 17th through August 23rd,
+     the following sales are in effect:</div>
+    <div>-Horror movie digital viewings are 30% off</div>
+    <div>-Horror movie tickets are 30% off</div>
+    <div>-Horror movie tickets booked at least two days in advance are 50% off!</div>
+    <div>-If you bring at least 3 friends, the tickets are a further 20% off!</div>
+    <div>You know what to do!</div>
+    </div>;
+    
+    
+
   const Graph2 = {  
     data:[{name:"BEP", sales:400}, {name:"LEG", sales:700}, {name:"Romance", sales:600}, {name:"DAWI-ZHARR", sales:1650}, {name:"Drama", sales:550}], 
     title: "WARLOCKS WITH GUNS", 
@@ -121,11 +195,47 @@ function Form1(){
   
 
 
-let preSortArticles=[article1, article2]
+  const article7={
+    TitleText: "T-Shirts with your favorite brand!",
+    PreviewText: "You love us, right? Then wear a t-shirt with our logo on it! You'll get a ticket discount!",
+    Date: new Date(2020, 8, 9),
+    Sections: ["HTML8"]
+    };
+
+
+
+  
+    const HTML8 = <div><div className="Temporary-Test">Head on over to the store 
+    and you can get a variety of shirts with our brand on it!</div>
+    <div>Over 20 colors, and in all sizes!</div>
+    <div>For every shirt you buy, you'll get a 10% off coupon for movie tickets!</div>
+    </div>;
+
+
+
+
+const article8={
+  TitleText: "Staff Changes",
+  PreviewText: "We've got some new hires, and some staff relocating.",
+  Date: new Date(2020, 8, 2),
+  Sections: ["HTML9"]
+  };
+
+
+
+
+  const HTML9 = <div><div className="Temporary-Test">We're welcoming Jace and Jesse 
+  as new hires for our Portland location!</div>
+  <div>Vanessa, our movie super-expert, is moving 
+    from our Portland location to our Spokane location. If you need to ask her questions, too bad!</div>
+  </div>;
+    
+
+let preSortArticles=[article1, article2, article3, article4, article5, article6, article7, article8]
 
 
 export let Articles=preSortArticles.sort(function(b,a){return a.Date.getTime() - b.Date.getTime()});
-export let HTMLs={"HTML1":HTML1, "HTML2":HTML2, "HTML3":HTML3}
+export let HTMLs={"HTML1":HTML1, "HTML2":HTML2, "HTML3":HTML3, "HTML4":HTML4, "HTML5":HTML5, "HTML6":HTML6, "HTML7":HTML7, "HTML8":HTML8, "HTML9":HTML9}
 export let Graphs={"Graph1":Graph1, "Graph2":Graph2}
 export let Forms=[Form1()]
 
