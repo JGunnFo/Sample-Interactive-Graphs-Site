14e17e7cecf67c964e1f2625bf6b5694fa2b0f3f
JGunnFo <jeffgunn814@gmail.com>
2020-09-15 14:51:06 -0700

    Fixed an issue with news preview buttons and space formatting.

--- src/subcomponents/newsDisplay.js
+++ src/subcomponents/newsDisplay.js
@@ -25,12 +25,13 @@ export  function sectionNews(props){
     
   function RenderSquares(passedPreview, props, index){
     return(
-      <div key={index} className="News-Square">
+      <div key={index}>
       <button className="Button-Unset" onClick={() => {props.dispatch(ArticleClick(passedPreview["Sections"]))}}>
+        <div className="News-Square">
       <div className="News-Title-Text">{passedPreview["TitleText"]}</div>
       <div className="News-Preview-Text" >{passedPreview["PreviewText"]}</div>
+        </div>
       </button>
       </div>
     )
   }
-
